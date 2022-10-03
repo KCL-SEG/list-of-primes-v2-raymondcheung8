1401fc77223281ee1a0dc06052c829190932d1e8
Raymond Cheung <raymond.cheung@kcl.ac.uk>
2022-10-03 21:52:23 +0100

    Attempt 1 at raising a ValueError

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+
+    if number_of_primes <= 0:
+        raise ValueError("The number entered must be above zero, exclusive!")
+
+    list.append(2)
+    nextNumber = 3
+
+    for i in range(number_of_primes - 1):
+        isNextPrime = False
+        while not isNextPrime:
+            for prime in list:
+                if nextNumber % prime == 0:
+                    break
+            else:
+                list.append(nextNumber)
+                isNextPrime = True
+
+            nextNumber += 2
+
     return list
